886a3d618cc31dc42a67b0f51f93be4cddefd5b3
Sumit Kumar Jha <42186301+sumitkjha@users.noreply.github.com>
2021-07-07 21:40:54 +0530

    Add files via upload

---
+++ drowsiness_detection.py
@@ -0,0 +1,224 @@
+#python drowniness_detection.py --webcam webcam_index
+
+
+#installing all pre-requisite libraries
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import os
+#import winsound
+
+
+#defining the alarm function
+def alarm(msg):
+    global alarm_status
+    global alarm_status2
+    global saying
+
+    while alarm_status:
+       # print('call')
+        s = 'espeak "'+msg+'"'
+        #os.system(s)
+
+    if alarm_status2:
+        #print('call')
+        saying = True
+        s = 'espeak "' + msg + '"'
+        #os.system(s)
+        saying = False
+
+#following two functions calculate the EAR ratio
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
+
+#this function calculates the lip_distance to detect yawning
+def lip_distance(shape):
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
+
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
+
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
+
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+                help="index of webcam on system")
+args = vars(ap.parse_args())
+
+
+EYE_AR_CONSEC_FRAMES = 30
+YAWN_THRESH = 12
+alarm_status = False
+alarm_status2 = False
+saying = False
+COUNTER = 0
+PBX1 = 0
+
+print("-> Loading the predictor and detector...")
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    #Faster but less accurate
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+print("-> Starting Video Stream")
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+an = 0
+ear1 = 0
+#main loop to start the program
+while(an<200):
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    an=an+1;
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+                                      minNeighbors=5, minSize=(30, 30),
+                                      flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        eye = final_ear(shape)
+        ear1 = ear1+eye[0]
+
+    cv2.putText(frame, "SCANNING", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 245), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+print("Scanning Complete")
+thres = ear1/an
+
+print("Drowsiness Detector Activated")
+EYE_AR_THRESH = thres
+print("Eye Aspect Ratio: ",EYE_AR_THRESH)
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+    ear = 0  # new change done here
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
+        
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        eye = final_ear(shape)
+        ear = eye[0]
+        leftEye = eye[1]
+        rightEye = eye[2]
+
+        distance = lip_distance(shape)
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (255, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 0), 0)
+
+        lip = shape[48:60]
+        cv2.drawContours(frame, [lip], -1, (0, 255, 255), 1)
+
+
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if alarm_status == False:
+                    alarm_status = True
+                    t = Thread(target=alarm, args=('Wake up, sir',))
+                    t.deamon = True
+                    t.start()
+
+                cv2.putText(frame, "Drowsiness Alert!", (30, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 245), 2)
+
+        else:
+            COUNTER = 0
+            alarm_status = False
+
+        if (distance > 18 ) : #YAWN_THRESH
+                cv2.putText(frame, "Yawn Alert!", (30, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 245), 2)
+
+
+
+
+                #cv2.putText(frame, "------", (10, 40),
+                           # cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+                if alarm_status2 == False and saying == False:
+                    alarm_status2 = True
+                    t = Thread(target=alarm, args=('take some fresh air sir',))
+                    t.deamon = True
+                    t.start()
+        else:
+            alarm_status2 = False
+
+        cv2.putText(frame, "EAR:{:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 245), 2)
+        cv2.putText(frame, "YAWN:{:.2f}".format(distance), (300, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 245), 2)
+
+        #x=leftEye[0]
+        #y=rightEye[0]
+        #print(x)
+        #cv2.line(frame, x, y,(0,0,255))
+    if ear == 0:
+       PBX1 += 1
+
+       if PBX1 >= 30:
+            cv2.putText(frame, "Focus On Driving", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 245), 2,)
+    else:
+        PBX1 = 0
+        alarm_status = False
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
